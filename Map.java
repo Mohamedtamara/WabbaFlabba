054e531a657a8eea9ae6a09322a95e4105eb533a
Alitquan <amallick@stuy.edu>
2017-01-15 20:04:52 +0000

    Updated Map so extra maps can have different sizes.

--- Map.java
+++ Map.java
@@ -6,54 +6,56 @@ public class Map {
     int mapLength;
     int mapWidth;
 
-    public ArrayList [] test = new ArrayList [11];
+    public ArrayList [] test = new ArrayList [20];
    
     public Map () {
-       	for (int i = 0; i < test.length; i ++) {
+	mapWidth = 11;
+       	for (int i = 0; i <mapWidth; i ++) {
 	    test [i] = new ArrayList <Character>();
 	}
-	for (int x = 0; x< test.length; x++) {
+	for (int x = 0; x<mapWidth; x++) {
 	    test[0].add('x');
-	    test[10].add('x');
+	    test[mapWidth-1].add('x');
 	}
-	for (int x = 1; x < test.length-1; x ++) {
+	for (int x = 1; x <mapWidth-1; x ++) {
 	    int filler = 0;
-	    while (filler < 11) {
+	    while (filler < mapWidth) {
 		test [x].add('*');
 		filler++;
 	    }
 	}
-	for (ArrayList o : test) {
-	    o.remove(0);
-	    o.remove(o.size()-1);
-	    o.add(0,'x');
-	    o.add('x');
+	for (int x = 0; x <mapWidth-1; x++) {
+	    test[x].remove(0);
+	    test[x].remove(test[x].size() - 1);
+	    test[x].add(0,'x');
+	    test[x].add('x');
 	}
-	mapLength = 11;
-	mapWidth = 11;
+	levelPlace(1,9);
+	
     }
 
-    public Map(int mapNum) {
+     public Map(int mapNum) {
 	if (mapNum == 1) {
-	    for (int i = 0; i < test.length; i ++) {
+	    mapWidth = 15;
+	    for (int i = 0; i <mapWidth; i ++) {
 		test [i] = new ArrayList <Character>();
 	    }
-	    for (int x = 0; x< test.length; x++) {
+	    for (int x = 0; x<mapWidth; x++) {
 		test[0].add('x');
-		test[10].add('x');
+		test[mapWidth-1].add('x');
 	    }
-	    for (int x = 1; x < test.length-1; x ++) {
+	    for (int x = 1; x <mapWidth-1; x ++) {
 		int filler = 0;
-		while (filler < 11) {
+		while (filler < mapWidth) {
 		    test [x].add('*');
 		    filler++;
 		}
 	    }
-	    for (ArrayList o : test) {
-		o.remove(0);
-		o.remove(o.size()-1);
-		o.add(0,'x');
-		o.add('x');
+	    for (int x = 0; x <mapWidth-1; x++) {
+		test[x].remove(0);
+		test[x].remove(test[x].size() - 1);
+		test[x].add(0,'x');
+		test[x].add('x');
 	    }
 	    swapPlace(9,2);
 	    swapPlace(8,2);
@@ -77,21 +79,29 @@ public class Map {
 	    swapPlace(9,7);
 	    swapPlace(3,7);
 	    swapPlace(3,9);
+	    levelPlace(13,13);
 	}
 	else {
 	    return;
 	}
+	mapWidth = 15;
     }
 	
     public void swapPlace(int index, int index2) {
 	test[index].remove(index2);
 	test[index].add(index2,'x');
     }
+
+    public void levelPlace (int index, int index2) {
+	test[index].remove(index2);
+	test[index].add(index2,'l');
+    }
+
     
     public String toString() {
 	String retStr = "";
 	int x = 0;
-	while (x < 11) {
+	while (x < mapWidth) {
 	    retStr += test [x];
 	    retStr += "\n";
 	    x++;
@@ -125,25 +135,25 @@ public class Map {
 	int oldPlayerYpos = playerYpos;
 	int oldPlayerXpos = playerXpos;
 	if (movementSignal == 1) {
-	    if ((char)test[playerYpos - 1].get(playerXpos) == 'x') {
+	    if ((char) test[playerYpos - 1].get(playerXpos) == 'x') {
 		System.out.println ("ERROR 404: access denied");
 		return;
 	    }
 	}
 	else if (movementSignal == 2) {
-	    if ((char)test[playerYpos].get(playerXpos+1) == 'x') {
+	    if ((char) test[playerYpos].get(playerXpos+1) == 'x') {
 		System.out.println ("ERROR 404: access denied");
 		return;
 	    }
 	}
 	else if (movementSignal == 3) {
-	    if ((char)test[playerYpos + 1].get(playerXpos) == 'x') {
+	    if ((char) test[playerYpos + 1].get(playerXpos) == 'x') {
 		System.out.println ("ERROR 404: access denied");
 		return;
 	    }
 	}
 	else if (movementSignal == 4) {
-	    if ((char)test[playerYpos].get(playerXpos - 1) == 'x') {
+	    if ((char) test[playerYpos].get(playerXpos - 1) == 'x') {
 		System.out.println ("ERROR 404: access denied");
 		return;
 	    }
@@ -196,12 +206,11 @@ public class Map {
         
 	
     public static void main (String [] args) {
-	//	ArrayList glen = new ArrayList ();
-	//	glen.add("Hello");
-	//    Map one = new Map ();
-	//	System.out.println (one);
+        Map one = new Map ();
+	System.out.println (one);
 	Map two = new Map (1);
 	System.out.println (two);
+	
 	
     }
 }
